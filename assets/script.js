b1b08669ba5b0e0efec7d107378c1c736f222a1e
RubabaKhandaker <140378374+RubabaKhandaker@users.noreply.github.com>
2023-09-18 23:46:30 +1000

    Added loadQuestion function to script.js + slightly edited style.css

--- assets/script.js
+++ assets/script.js
@@ -15,10 +15,10 @@ var Questions = [
 },
 {
     q: "The condition in an if/else statement is enclosed with ______.",
-    a: [{ text: "Quotes", isCorrect: false, isSelected: false },
+    a: [{ text: "Quotes", isCorrect: false, },
     { text: "Curly Brackets", isCorrect: false },
-    { text: "Parenthesis", isCorrect: false },
-    { text: "Square Brackets", isCorrect: true }
+    { text: "Parenthesis", isCorrect: true },
+    { text: "Square Brackets", isCorrect: false }
     ]
  
 },
@@ -26,8 +26,8 @@ var Questions = [
     q: "Arrays in JavaScript can be used to store ______.",
     a: [{ text: "Numbers and Strings", isCorrect: false },
     { text: "Other Arrays", isCorrect: false },
-    { text: "Booleans", isCorrect: true },
-    { text: "All of The Above", isCorrect: false }
+    { text: "Booleans", isCorrect: false },
+    { text: "All of The Above", isCorrect: true }
     ]
  
 },
@@ -40,6 +40,46 @@ var Questions = [
     { text: "Parenthesis", isCorrect: false }
     ]
 
+},
+{
+
+    q: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    a: [{ text: "JavaScript", isCorrect: true },
+    { text: "Terminal/Bash", isCorrect: false },
+    { text: "for Loops", isCorrect: false },
+    { text: "console.log", isCorrect: false }
+    ]
+
 }
  
 ]
+
+// Loads questions on to webpage
+function loadQuestion() {
+    var question = document.getElementById("questID")
+    var option = document.getElementById("optID")
+ 
+    question.textContent = Questions[currentQuest].q;
+    option.innerHTML = ""
+ 
+    for (let i = 0; 
+         i < Questions[currentQuest].a.length;
+         i++) {
+
+        var choicesDiv = document.createElement("div");
+        var choices = document.createElement("input");
+        var choicesLabel = document.createElement("label");
+ 
+        choices.type = "radio";
+        choices.name = "answer";
+        choices.value = i;
+ 
+        choicesLabel.textContent = Questions[currentQuest].a[i].text;
+ 
+        choicesDiv.appendChild(choices);
+        choicesDiv.appendChild(choicesLabel);
+        option.appendChild(choicesDiv);
+    }
+}
+ 
+loadQuestion();
