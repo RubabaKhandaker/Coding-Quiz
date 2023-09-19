dea5cd60090ed649cb19f0a5fa363503cca7171a
RubabaKhandaker <140378374+RubabaKhandaker@users.noreply.github.com>
2023-09-19 23:23:08 +1000

    Scrapped majority of javascript code and some html code to make room for fresh code

--- assets/script.js
+++ assets/script.js
@@ -1,105 +1,52 @@
-let score = 0 //Stores score
-let currentQuest = 0 //Starting question
-
 // Quiz questions
 var Questions = [
     
 {
     q: "Commonly used data types DO NOT include:",
-    a: [{ text: "Strings", isCorrect: false },
-    { text: "Booleans", isCorrect: false },
-    { text: "Alerts", isCorrect: true },
-    { text: "Numbers", isCorrect: false }
+    a: [{ text: "Strings", correct: false },
+    { text: "Booleans", correct: false },
+    { text: "Alerts", correct: true },
+    { text: "Numbers", correct: false }
     ]
  
 },
 {
     q: "The condition in an if/else statement is enclosed with ______.",
-    a: [{ text: "Quotes", isCorrect: false, },
-    { text: "Curly Brackets", isCorrect: false },
-    { text: "Parenthesis", isCorrect: true },
-    { text: "Square Brackets", isCorrect: false }
+    a: [{ text: "Quotes", correct: false, },
+    { text: "Curly Brackets", correct: false },
+    { text: "Parenthesis", correct: true },
+    { text: "Square Brackets", correct: false }
     ]
  
 },
 {
     q: "Arrays in JavaScript can be used to store ______.",
-    a: [{ text: "Numbers and Strings", isCorrect: false },
-    { text: "Other Arrays", isCorrect: false },
-    { text: "Booleans", isCorrect: false },
-    { text: "All of The Above", isCorrect: true }
+    a: [{ text: "Numbers and Strings", correct: false },
+    { text: "Other Arrays", correct: false },
+    { text: "Booleans", correct: false },
+    { text: "All of The Above", correct: true }
     ]
  
 },
 {
 
     q: "String values must be enclosed within ______ when being assigned to variables.",
-    a: [{ text: "Commas", isCorrect: false },
-    { text: "Curly Brackets", isCorrect: false },
-    { text: "Quotes", isCorrect: true },
-    { text: "Parenthesis", isCorrect: false }
+    a: [{ text: "Commas", correct: false },
+    { text: "Curly Brackets", correct: false },
+    { text: "Quotes", correct: true },
+    { text: "Parenthesis", correct: false }
     ]
 
 },
 {
 
     q: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    a: [{ text: "JavaScript", isCorrect: true },
-    { text: "Terminal/Bash", isCorrect: false },
-    { text: "for Loops", isCorrect: false },
-    { text: "console.log", isCorrect: false }
+    a: [{ text: "JavaScript", correct: true },
+    { text: "Terminal/Bash", correct: false },
+    { text: "for Loops", correct: false },
+    { text: "console.log", correct: false }
     ]
 
 }
  
 ]
-
-// Loads questions on to webpage
-function loadQuestion() {
-    var question = document.getElementById("questID")
-    var option = document.getElementById("optID")
- 
-    question.textContent = Questions[currentQuest].q;
-    option.innerHTML = ""
- 
-    for (let i = 0; 
-         i < Questions[currentQuest].a.length;
-         i++) {
-
-        var choicesDiv = document.createElement("div");
-        var choices = document.createElement("input");
-        var choicesLabel = document.createElement("label");
- 
-        choices.type = "radio";
-        choices.name = "answer";
-        choices.value = i;
- 
-        choicesLabel.textContent = Questions[currentQuest].a[i].text;
- 
-        choicesDiv.appendChild(choices);
-        choicesDiv.appendChild(choicesLabel);
-        option.appendChild(choicesDiv);
-    }
-}
- 
-loadQuestion();
-
-
-// Moves on to next question after answering
-function nextQuestion() {
-    if (currentQuest < Questions.length - 1) {
-        currentQuest++;
-        loadQuestion();
-    } else {
-        document.getElementById("optClass").remove()
-        document.getElementById("questClass").remove()
-        document.getElementById("submitBtn").remove()
-        loadScore();
-    }
-}
-
-// Loads score at the end of quiz
-function loadScore() {
-    const totalScore = document.getElementById("scores")
-    totalScore.textContent = ["You scored " + scores + "out of " + Questions.length + "."];
-}
