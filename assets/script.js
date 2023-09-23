4df73d1a44c041e7dfebb0b436b6ca7ebd17b3cc
RubabaKhandaker <140378374+RubabaKhandaker@users.noreply.github.com>
2023-09-23 23:43:18 +1000

    Added some JS to script.js

--- assets/script.js
+++ assets/script.js
@@ -50,3 +50,28 @@ var Questions = [
 }
  
 ]
+
+
+// var to refrence objects with specified Id's in HTML
+
+var countdown = 75;
+var timerID;
+var timerIDEl = document.getElementById("timerID");
+var startButtonID = document.getElementById("startBtnID");
+var nextButton = document.getElementById("nextButtonID");
+var questionsIDEl = document.getElementById("questionsID");
+var startingPageIDEl = document.getElementById("startingPageID");
+var questionsEl = document.getElementById("questions");
+var answerButtonsIDEl = document.getElementById("answerButtonsID");
+var answerCheckIDEl = document.getElementById("answerCheckID");
+var submitButtonID = document.getElementById("submitButtonID");
+var clearButtonID = document.getElementById("clearButtonID");
+var restartButtonID = document.getElementById("restartButtonID");
+var playerScores = document.getElementById("player-score");
+var scoreView = document.getElementById("highscores-link");
+var playerInitials = document.getElementById("player-name");
+var scores = JSON.parse(localStorage.getItem("scores")) || [];
+
+var randomQuestion, presentQuestion;
+
+
